e90d55195d045dd163b5edc13e3eb1da202d76f2
kikuchi <kikuchi@lepidum.co.jp>
2018-02-28 22:18:38 +0900

    oops

--- core/src/main/java/org/bitcoinj/params/BSafeNetLT5Params.java
+++ core/src/main/java/org/bitcoinj/params/BSafeNetLT5Params.java
@@ -1,5 +1,6 @@
 package org.bitcoinj.params;
 
+import org.bitcoinj.core.ArchiveHashParameters;
 import org.bitcoinj.core.Utils;
 
 public class BSafeNetLT5Params extends BSafeNetParams {
@@ -8,6 +9,14 @@ public class BSafeNetLT5Params extends BSafeNetParams {
         id = ID_BSAFENETLT5;
         packetMagic += 0x50L << 24;
         port += 500;
+
+        newPoWHashStartHeight = 100;
+
+        archiveHashParameters = new ArchiveHashParameters[1];
+        archiveHashParameters[0] = new ArchiveHashParameters();
+        archiveHashParameters[0].startHeight = 200;
+        archiveHashParameters[0].blocksPerHash = 10;
+        archiveHashParameters[0].nBlocks = 10;
     }
 
     private static BSafeNetLT5Params instance;
